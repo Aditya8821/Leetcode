d96d3ed34eb70c1c5b5c509a0ad6705d6f93617a
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-02-09 23:59:34 +0530

    Time: 42 ms (75.02%), Space: 14.3 MB (83.36%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            leftVal = nums[left]
+            start = nums[left]
+            midVal = nums[mid]
+            if midVal == target:
+                return mid
+        
+            if (leftVal < midVal and leftVal <= target < midVal):
+                # leftVal and midVal are within one section
+                # target is in the current section
+                right = mid - 1
+            elif (leftVal > midVal and (target < midVal or target >= leftVal)):
+                # leftVal and midVal span both sections, but
+                # target is between them
+                right = mid - 1    
+            else:
+                # target is not in the current range
+                left = mid + 1
+        return -1
