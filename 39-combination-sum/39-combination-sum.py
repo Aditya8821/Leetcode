9217497451d539b0bd6aacc57a9ded8d52ea5ce5
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-02-19 10:43:07 +0530

    Time: 110 ms (51.65%), Space: 14 MB (82.65%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,15 +1,35 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans=[]
-        def solve(candidates,tar,res,idx):
-            if tar==0:
-                ans.append(res[:])
+        final=[]
+        def solve(index,candidates,tar,final,res):
+            if index==len(candidates):
+                if tar==0:
+                    final.append(res.copy())
+                    return 
                 return
-            if tar<0:
-                return
-            for index in range(idx,len(candidates)):
+            
+            if candidates[index]<=tar:
                 res.append(candidates[index])
-                solve(candidates,tar-candidates[index],res,index)
+                solve(index,candidates,tar-candidates[index],final,res)
                 res.pop()
-        solve(candidates,target,[],0)
-        return ans
+            solve(index+1,candidates,tar,final,res)
+        solve(0,candidates,target,final,[])
+        return final
+        
+        
+        
+        
+        
+        # ans=[]
+        # def solve(candidates,tar,res,idx):
+        #     if tar==0:
+        #         ans.append(res[:])
+        #         return
+        #     if tar<0:
+        #         return
+        #     for index in range(idx,len(candidates)):
+        #         res.append(candidates[index])
+        #         solve(candidates,tar-candidates[index],res,index)
+        #         res.pop()
+        # solve(candidates,target,[],0)
+        # return ans
