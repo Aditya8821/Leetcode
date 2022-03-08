bf045b0ecba53b4b31e68af6f44360bca3ebf8cb
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-03-08 19:00:23 +0530

    Time: 581 ms (77.72%), Space: 19 MB (36.34%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        minS = (1 << 31)
+        minSum = 0
+        maxS = -(1 << 31)
+        totSum = 0
+        maxSum = 0
+        for ele in nums:
+            totSum += ele
+            maxSum += ele
+            maxS = max(maxS, maxSum)
+            if maxSum < 0:
+                maxSum = 0
+			# ...
+            minSum += ele
+            minS = min(minS, minSum)
+            if minSum > 0:
+                minSum = 0
+        if(totSum == minS):
+            return maxS
+        return max(maxS, (totSum-minS))
