e1677f9aa2cb85d49e8487aea282c35781d93ca3
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-03-30 12:09:51 +0530

    Time: 159 ms (23.69%), Space: 14.1 MB (29.09%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,35 +1,35 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        final=[]
-        def solve(index,candidates,tar,final,res):
-            if index==len(candidates):
-                if tar==0:
-                    final.append(res.copy())
-                    return 
-                return
+#         final=[]
+#         def solve(index,candidates,tar,final,res):
+#             if index==len(candidates):
+#                 if tar==0:
+#                     final.append(res.copy())
+#                     return 
+#                 return
             
-            if candidates[index]<=tar:
+#             if candidates[index]<=tar:
+#                 res.append(candidates[index])
+#                 solve(index,candidates,tar-candidates[index],final,res)
+#                 res.pop()
+#             solve(index+1,candidates,tar,final,res)
+#         solve(0,candidates,target,final,[])
+#         return final
+        
+        
+        
+        
+        
+        ans=[]
+        def solve(candidates,tar,res,idx):
+            if tar==0:
+                ans.append(res[:])
+                return
+            if tar<0:
+                return
+            for index in range(idx,len(candidates)):
                 res.append(candidates[index])
-                solve(index,candidates,tar-candidates[index],final,res)
+                solve(candidates,tar-candidates[index],res,index)
                 res.pop()
-            solve(index+1,candidates,tar,final,res)
-        solve(0,candidates,target,final,[])
-        return final
-        
-        
-        
-        
-        
-        # ans=[]
-        # def solve(candidates,tar,res,idx):
-        #     if tar==0:
-        #         ans.append(res[:])
-        #         return
-        #     if tar<0:
-        #         return
-        #     for index in range(idx,len(candidates)):
-        #         res.append(candidates[index])
-        #         solve(candidates,tar-candidates[index],res,index)
-        #         res.pop()
-        # solve(candidates,target,[],0)
-        # return ans
+        solve(candidates,target,[],0)
+        return ans
