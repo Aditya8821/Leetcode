a0776dd46edc933061074e5de68e6d8d802af9cf
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-04-18 12:17:51 +0530

    Time: 54 ms (83.85%), Space: 18 MB (48.12%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -7,19 +7,19 @@
 import heapq
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # 0. base case, if a root doesn't exist, return None
-        if not root:
-            return None
-        # 1. set up an answer array and heapify it
-        answer = []
-        heapq.heapify(answer)
-        # 2. set up a helper function that takes in a node and heappushes the value of that node (if it exists) and then calls the helper function for the left and right brances of the node
-        def helper(node):
-            if node:
-                heapq.heappush(answer, node.val)
-                helper(node.left)
-                helper(node.right)
-        # 3. initialize the helper function
-        helper(root)
-        # 4. return the last value from the heap using the k smallest values
-        return heapq.nsmallest(k, answer)[-1]
+        answer=[]
+        def solve(root):
+            if not root:
+                return
+            solve(root.left)
+            answer.append(root.val)
+            solve(root.right)
+        solve(root)
+        heapify(answer)
+        
+        for i in range(1,k+1):
+            if i is k:
+                value = heappop(answer)
+                break
+            heappop(answer)
+        return value
