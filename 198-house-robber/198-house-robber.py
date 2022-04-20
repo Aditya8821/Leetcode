f0791f0d557a42d55c3a29fec60f02766fceacf5
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-04-20 11:21:11 +0530

    Time: 30 ms (90.83%), Space: 13.9 MB (69.74%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n<=2:
-            return max(nums)
-        dp=[0]*n
+        l=len(nums)
+        if l<=2: return max(nums)
+        dp=[0]*l
         dp[0],dp[1]=nums[0],max(nums[0],nums[1])
-        for i in range(2,n):
+        for i in range(2,l):
             dp[i]=max(nums[i]+dp[i-2],dp[i-1])
-        return dp[n-1]
+        return dp[-1]
+        
+        
+        # n=len(nums)
+        # if n<=2:
+        #     return max(nums)
+        # dp=[0]*n
+        # dp[0],dp[1]=nums[0],max(nums[0],nums[1])
+        # for i in range(2,n):
+        #     dp[i]=max(nums[i]+dp[i-2],dp[i-1])  #here dp[i] is storing the maximum money in the order of leaving one house
+        # return dp[n-1]
         
         
         
