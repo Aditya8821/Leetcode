b5fc7cbf80c4a0cf22e76876f50396e37166b20d
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-06-30 12:04:44 +0530

    Time: 109 ms (55.38%), Space: 15.4 MB (79.18%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,26 +1,29 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
-        if len(nums)%2==0:
-            mid=(len(nums)//2)-1
-        else:
-            mid=len(nums)//2
         s=0
-        midele=nums[mid]
+        midele=nums[len(nums)//2]
         for num in nums:
-            if num<midele: 
-                s+=midele-num
-            else:
-                s+=num-midele
+            s+=abs(midele-num)
         return s
+            
         
         
-        
-        
-        # minele=min(nums)
-        # maxele=max(nums)
-        # i,j=0,0
+        # nums.sort()
+        # if len(nums)%2==0:
+        #     mid=(len(nums)//2)-1
+        # else:
+        #     mid=len(nums)//2
+        # s=0
+        # midele=nums[mid]
         # for num in nums:
-        #     i+=num-minele
-        #     j+=maxele-num
-        # return min(i,j)
+        #     if num<midele: 
+        #         s+=midele-num
+        #     else:
+        #         s+=num-midele
+        # return s
+        
+        
+        
+        
+     
