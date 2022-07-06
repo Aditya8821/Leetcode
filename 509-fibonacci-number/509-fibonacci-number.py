9942783ecc0221d765adb04e04ee3abc2662678b
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-07-06 18:27:57 +0530

    Time: 51 ms (52.11%), Space: 13.8 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -3,7 +3,7 @@ class Solution:
         if n==0: return 0
         if n==1 or n==2: return 1
         dp=[0]*(n+1)
-        dp[1],dp[2]=1,1
-        for i in range(3,n+1):
+        dp[1]=1
+        for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
