9be31c0c81f26e697bc6ffc0cde0bf08d519583a
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-08-02 09:55:03 +0530

    Time: 101 ms (89.59%), Space: 14.2 MB (25.87%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -4,6 +4,8 @@ class Solution:
         nums1.sort()
         n=len(nums1)
         if n%2!=0:
-            return nums1[int(n/2)]
-        x=nums1[int(n/2)] + nums1[int(n/2)-1]
+            return nums1[int(n//2)]
+        x=nums1[int(n/2)]+ nums1[int(n/2)-1]
         return x/2
+         
+        
