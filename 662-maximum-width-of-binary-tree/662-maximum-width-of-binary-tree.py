4722a9ee4aa77da3bfc63b44d7afe2ecf31ce954
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-08-04 14:53:07 +0530

    Time: 49 ms (88.82%), Space: 14.8 MB (74.97%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -6,11 +6,30 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        di = defaultdict(list)
-        def dfs(node, level, column):
-            if node:
-                di[level].append(column)
-                dfs(node.left, level+1, column*2)
-                dfs(node.right, level+1, column*2+1)
-        dfs(root, 0 , 0)
-        return max([max(di[level]) - min(di[level]) +1 for level in di])
+        queue=[(root,0)]
+        maxdepth=0
+        while queue:
+            n=len(queue)
+            for i in range(n):
+                node,curr=queue.pop(0)
+                if i==0:
+                    start=curr
+                if i==n-1:
+                    end=curr
+                if node.left:
+                    queue.append((node.left,2*curr+1))
+                if node.right:
+                    queue.append((node.right,2*curr+2))
+            maxdepth=max(maxdepth,end-start+1)
+        return maxdepth
+        
+        
+        
+        # di = defaultdict(list)
+        # def dfs(node, level, column):
+        #     if node:
+        #         di[level].append(column)
+        #         dfs(node.left, level+1, column*2)
+        #         dfs(node.right, level+1, column*2+1)
+        # dfs(root, 0 , 0)
+        # return max([max(di[level]) - min(di[level]) +1 for level in di])
