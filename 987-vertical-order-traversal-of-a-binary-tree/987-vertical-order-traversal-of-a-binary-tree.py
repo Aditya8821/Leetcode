76d88c948ef240246bd5b46d7be534b852c49c9c
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-08-04 06:11:28 +0530

    Time: 60 ms (33.65%), Space: 14.1 MB (72.09%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,28 +6,22 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue = [(root, 0)]
-        hashmap = collections.defaultdict(list);
-        level = 0
-        while (len(queue)):
-            levelSize = len(queue)
-            while (levelSize > 0):
-                curElem, pos = queue.pop(0)
-                hashmap[pos].append((level, curElem.val));
-                
-                if (curElem.left):
-                    queue.append((curElem.left, pos-1))
-                
-                if (curElem.right):
-                    queue.append((curElem.right, pos+1));
-                    
-                levelSize-=1;
-            level+=1;
-        keys = sorted(hashmap.keys());
-        result = []
-            
-        for key in keys:
-            curLevelElems = hashmap[key];
-            curLevelElems = sorted(hashmap[key])
-            result.append(list(map(lambda x: x[1],curLevelElems)));
-        return result
+        res=[]
+        d=collections.defaultdict(list)
+        temp=[(root,0)]
+        while temp:
+            next=[]
+            for value,pos in temp:
+                d[pos].append(value.val)
+                if value.left: next.append((value.left,pos-1))
+                if value.right: next.append((value.right,pos+1))
+                next.sort(key = lambda x: (x[1],x[0].val)) #the next will sort according to x[0].val in cases like example 2
+            temp=next
+        for k in sorted(d):
+            res.append(d[k])
+        return res
+        
+        
+        
+         
+        
