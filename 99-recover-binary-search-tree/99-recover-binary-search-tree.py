523b31a6c55da158ead4421be90de2d128c447f5
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-08-20 19:43:11 +0530

    Time: 113 ms (50.19%), Space: 14.2 MB (64.92%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -5,28 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def recoverTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: None Do not return anything, modify root in-place instead.
-        """
-        self.first, self.second, self.prevNode = None, None, None # Create three nodes.
-        self.inOrder(root) # Calling the function
-        self.first.val, self.second.val = self.second.val, self.first.val 
-        # Swapping the two elements needed to be swapped
-        
-    def inOrder(self, root):
+    def inorder(self,root):
         if not root:
             return
-        self.inOrder(root.left)
+        self.inorder(root.left)
+        if self.prevnode:
+            if self.prevnode.val>root.val:
+                if not self.first:
+                    self.first=self.prevnode
+                self.second=root
+        self.prevnode=root
+        self.inorder(root.right)
         
-        if self.prevNode: # To handle the case of first node, because we make it prev to begin with
-            if self.prevNode.val > root.val: # Check property violation
-                if not self.first: 
-                    self.first = self.prevNode # Found first pair
-                self.second = root # If the second pair is found then simply assign the smaller element of the   pair as the second guy, it works for single pair easily, as it wont get             updated again in that case.
-                
-        self.prevNode = root
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.first,self.second,self.prevnode=None,None,None
+        self.inorder(root)
+        self.first.val,self.second.val=self.second.val,self.first.val
         
-        self.inOrder(root.right)
-        
+    
