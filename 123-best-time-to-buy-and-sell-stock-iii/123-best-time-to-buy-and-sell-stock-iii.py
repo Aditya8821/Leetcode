7c7a03d45e8a508c9df06d69357e6992f639f7cc
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-09-12 16:55:32 +0530

    Time: 2858 ms (34.29%), Space: 28.3 MB (80.43%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,20 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        profits = []
-        max_profit = 0
-        current_min = prices[0]
+        if not prices: return []
+        maxprofit=0
+        profit=[]
+        currmin=prices[0]
         for price in prices:
-            current_min = min(current_min, price)
-            max_profit = max(max_profit, price - current_min)
-            profits.append(max_profit) 
-        total_max = 0    
-        max_profit = 0
-        current_max = prices[-1]
-        for i in range(len(prices) - 1, -1, -1):
-            current_max = max(current_max, prices[i])
-            max_profit = max(max_profit, current_max - prices[i])
-            total_max = max(total_max, max_profit + profits[i])
-
-        return total_max
+            currmin=min(currmin,price)
+            maxprofit=max(maxprofit,price-currmin)
+            profit.append(maxprofit)
+        totalprofit=0
+        maxprofit=0
+        currmax=prices[-1]
+        for i in range(len(prices)-1,-1,-1):
+            currmax=max(currmax,prices[i])
+            maxprofit=max(maxprofit,currmax-prices[i])
+            totalprofit=max(totalprofit,maxprofit+profit[i])
+        return totalprofit
+       
+      
