46ec7a68ecb5b06bcf2743567f09946556ee9e6f
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-09-18 18:37:42 +0530

    Time: 5446 ms (14.07%), Space: 16.9 MB (98.77%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums=sorted(nums)
+        nums.sort()
         res=set()
         for i in range(len(nums)):
             l=i+1
@@ -16,6 +16,25 @@ class Solution:
                 else:
                     r-=1
         return list(res)
+        
+        
+        
+        # nums=sorted(nums)
+        # res=set()
+        # for i in range(len(nums)):
+        #     l=i+1
+        #     r=len(nums)-1
+        #     target=-nums[i]
+        #     while l<r:
+        #         if nums[l]+nums[r]==target:
+        #             res.add((nums[i],nums[l],nums[r]))
+        #             l+=1
+        #             r-=1
+        #         elif nums[l]+nums[r]<target:
+        #             l+=1
+        #         else:
+        #             r-=1
+        # return list(res)
         
         
         
