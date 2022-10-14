32b258bc26e71ff6547529c776f5fd2c60cf579f
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-10-15 01:54:35 +0530

    Time: 2077 ms (84.72%), Space: 60.7 MB (11.80%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -5,30 +5,14 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #for removing that extra prev pointer from below 1st approach
         if head.next==None:
             return None
-        slow,fast=head,head.next.next
-        while fast!=None and fast.next!=None:
+        prev,slow,fast=head,head,head
+        while fast and fast.next:
+            prev=slow
             slow=slow.next
             fast=fast.next.next
-        temp=slow.next
-        slow.next=slow.next.next
+        temp=prev.next
+        prev.next=prev.next.next
         del temp
         return head
-        
-        
-        
-        
-        # 1st approach
-        # if head.next==None:
-        #     return None
-        # prev,slow,fast=head,head,head
-        # while fast!=None and fast.next!=None:
-        #     prev=slow
-        #     slow=slow.next
-        #     fast=fast.next.next
-        # temp=prev.next
-        # prev.next=prev.next.next
-        # del temp
-        # return head
